f0f6a9e5f745067bf560b7541b31d4b6e642263f
김범진 <sdpthf@naver.com>
2021-12-21 19:09:26 +0900

    📑 :: LoginRequest

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/user/api/dto/request/LoginRequest.java
@@ -0,0 +1,18 @@
+package team.sb.authorizationserver.domain.user.api.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "email은 Null, 공백을 허용하지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "password는 Null, 공백을 허용하지 않습니다.")
+    private String password;
+
+}
