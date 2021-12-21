174527b80ec084ac5072b64e59df0f5f41e8bea5
김범진 <sdpthf@naver.com>
2021-12-21 13:51:56 +0900

    📑 :: ExceptionFilter

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/error/exception/ExceptionFilter.java
@@ -0,0 +1,30 @@
+package team.sb.authorizationserver.global.error.exception;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import team.sb.authorizationserver.global.error.ErrorResponse;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (SBException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getStatus(), e.getErrorCode().getMessage());
+
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertToJson(errorResponse));
+        }
+
+    }
+
+}
