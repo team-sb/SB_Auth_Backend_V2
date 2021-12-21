c72966dd8896abc1d1fde2febaae1e93f13755f9
김범진 <sdpthf@naver.com>
2021-12-21 15:52:22 +0900

    📑 :: UserAlreadyExistsException

--- authorization-server/src/main/java/team/sb/authorizationserver/global/error/ErrorCode.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/error/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     EXPIRED_TOKEN(401, "Expired Token"),
     INVALID_TOKEN(401, "Invalid Token"),
 
-    USER_NOT_FOUND(404, "User Not Found");
+    USER_NOT_FOUND(404, "User Not Found"),
+    USER_ALREADY_EXISTS(409, "User Already Exists");
 
     private final int status;
     private final String message;
