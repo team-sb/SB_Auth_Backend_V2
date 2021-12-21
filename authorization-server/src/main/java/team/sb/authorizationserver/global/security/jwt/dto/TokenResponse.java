a2c1e1e43a45a65c6b46500f28d47d6cb2e3727e
김범진 <sdpthf@naver.com>
2021-12-21 13:21:22 +0900

    📑 :: TokenResponse

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/security/jwt/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package team.sb.authorizationserver.global.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+}
