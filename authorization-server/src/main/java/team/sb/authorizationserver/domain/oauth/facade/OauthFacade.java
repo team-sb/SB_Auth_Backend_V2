458f72906b8f27c62158ea68d568399c400ed7b5
김범진 <sdpthf@naver.com>
2021-12-23 13:53:09 +0900

    📑 :: OauthFacade

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/oauth/facade/OauthFacade.java
@@ -0,0 +1,46 @@
+package team.sb.authorizationserver.domain.oauth.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import team.sb.authorizationserver.domain.oauth.api.dto.ClientDto;
+import team.sb.authorizationserver.domain.oauth.entity.OauthCode;
+import team.sb.authorizationserver.domain.oauth.entity.OauthDetails;
+import team.sb.authorizationserver.domain.oauth.exception.ClientNotFoundException;
+import team.sb.authorizationserver.domain.oauth.repository.OauthCodeRepository;
+import team.sb.authorizationserver.domain.oauth.repository.OauthDetailsRepository;
+import team.sb.authorizationserver.global.util.AuthUtil;
+
+@RequiredArgsConstructor
+@Component
+public class OauthFacade {
+
+    private final AuthUtil authUtil;
+    private final OauthCodeRepository oauthCodeRepository;
+    private final OauthDetailsRepository oauthDetailsRepository;
+
+    public ClientDto getClientDetails() {
+        String clientId = authUtil.getRandomCode(11);
+        String clientSecret = authUtil.getRandomCode(11);
+
+        return new ClientDto(clientId, clientSecret);
+    }
+
+    public OauthDetails getDetailsByClientId(String clientId) {
+        return oauthDetailsRepository.findById(clientId)
+                .orElseThrow(() -> ClientNotFoundException.EXCEPTION);
+    }
+
+    public OauthCode getCodeByClientId(String clientId) {
+        return oauthCodeRepository.findById(clientId)
+                .orElseThrow(() -> ClientNotFoundException.EXCEPTION);
+    }
+
+    public void newOauthCode(String clientId, String code) {
+        oauthCodeRepository.save(
+                new OauthCode(
+                        clientId, code
+                )
+        );
+    }
+
+}
