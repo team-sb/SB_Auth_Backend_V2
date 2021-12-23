d3d781ec433d2189e854e4761f08436b3eff7060
김범진 <sdpthf@naver.com>
2021-12-23 13:52:55 +0900

    📑 :: OauthService

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/oauth/service/OauthServiceImpl.java
@@ -0,0 +1,55 @@
+package team.sb.authorizationserver.domain.oauth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.sb.authorizationserver.domain.oauth.api.dto.ClientDto;
+import team.sb.authorizationserver.domain.oauth.entity.OauthCode;
+import team.sb.authorizationserver.domain.oauth.entity.OauthDetails;
+import team.sb.authorizationserver.domain.oauth.exception.InvalidClientSecret;
+import team.sb.authorizationserver.domain.oauth.exception.InvalidOauthCodeException;
+import team.sb.authorizationserver.domain.oauth.facade.OauthFacade;
+import team.sb.authorizationserver.domain.oauth.repository.OauthDetailsRepository;
+import team.sb.authorizationserver.global.security.jwt.JwtTokenProvider;
+import team.sb.authorizationserver.global.security.jwt.dto.TokenResponse;
+
+@RequiredArgsConstructor
+@Service
+public class OauthServiceImpl implements OauthService {
+
+    private final OauthFacade oauthFacade;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final OauthDetailsRepository oauthDetailsRepository;
+
+    @Transactional
+    @Override
+    public ClientDto registerClient() {
+        ClientDto response = oauthFacade.getClientDetails();
+
+        oauthDetailsRepository.save(
+                new OauthDetails(
+                        response.getClientId(), response.getClientSecret()
+                )
+        );
+
+        return response;
+    }
+
+    @Override
+    public TokenResponse getToken(String code, ClientDto clientDto) {
+        String clientId = clientDto.getClientId();
+        OauthDetails oauthDetails = oauthFacade.getDetailsByClientId(clientId);
+        OauthCode oauthCode = oauthFacade.getCodeByClientId(clientId);
+
+        if(!oauthDetails.getClientSecret().equals(clientDto.getClientSecret())) {
+            throw InvalidClientSecret.EXCEPTION;
+        }
+
+        if(!oauthCode.getCode().equals(code)) {
+            throw InvalidOauthCodeException.EXCEPTION;
+        }
+
+        return jwtTokenProvider.generateToken(code);
+    }
+
+}
--- authorization-server/src/main/java/team/sb/authorizationserver/domain/user/service/UserServiceImpl.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/user/service/UserServiceImpl.java
@@ -5,21 +5,15 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.sb.authorizationserver.domain.oauth.api.dto.ClientDto;
-import team.sb.authorizationserver.domain.oauth.entity.OauthCode;
+import team.sb.authorizationserver.domain.authcode.facade.AuthCodeFacade;
 import team.sb.authorizationserver.domain.oauth.entity.OauthDetails;
-import team.sb.authorizationserver.domain.oauth.exception.InvalidClientSecret;
-import team.sb.authorizationserver.domain.oauth.exception.InvalidOauthCodeException;
 import team.sb.authorizationserver.domain.oauth.facade.OauthFacade;
 import team.sb.authorizationserver.domain.user.api.dto.request.EmailRequest;
 import team.sb.authorizationserver.domain.user.api.dto.request.LoginRequest;
 import team.sb.authorizationserver.domain.user.api.dto.request.SignupRequest;
-import team.sb.authorizationserver.domain.authcode.facade.AuthCodeFacade;
 import team.sb.authorizationserver.domain.user.entity.User;
 import team.sb.authorizationserver.domain.user.exception.InvalidPasswordException;
 import team.sb.authorizationserver.domain.user.facade.UserFacade;
-import team.sb.authorizationserver.global.security.jwt.JwtTokenProvider;
-import team.sb.authorizationserver.global.security.jwt.dto.TokenResponse;
 import team.sb.authorizationserver.global.util.AuthUtil;
 
 @RequiredArgsConstructor
@@ -31,7 +25,6 @@ public class UserServiceImpl implements UserService {
     private final OauthFacade oauthFacade;
     private final AuthCodeFacade authCodeFacade;
     private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     @Override
@@ -60,23 +53,6 @@ public class UserServiceImpl implements UserService {
         oauthFacade.newOauthCode(clientId, code);
 
         return code;
-    }
-
-    @Override
-    public TokenResponse getToken(String code, ClientDto clientDto) {
-        String clientId = clientDto.getClientId();
-        OauthDetails oauthDetails = oauthFacade.getDetailsByClientId(clientId);
-        OauthCode oauthCode = oauthFacade.getCodeByClientId(clientId);
-
-        if(!oauthDetails.getClientSecret().equals(clientDto.getClientSecret())) {
-            throw InvalidClientSecret.EXCEPTION;
-        }
-
-        if(!oauthCode.getCode().equals(code)) {
-            throw InvalidOauthCodeException.EXCEPTION;
-        }
-
-        return jwtTokenProvider.generateToken(code);
     }
 
 }
