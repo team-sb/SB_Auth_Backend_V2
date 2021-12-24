4f31ae12841d733c526c07e7c0cb1ff6b7357fa9
김범진 <sdpthf@naver.com>
2021-12-24 09:39:58 +0900

    📑 :: S3Config

---
+++ resource-server/src/main/java/team/sb/resourceserver/global/properties/AwsProperties.java
@@ -0,0 +1,17 @@
+package team.sb.resourceserver.global.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "cloud.aws.credentials")
+public class AwsProperties {
+
+    private final String accessKey;
+    private final String secretKey;
+
+}
