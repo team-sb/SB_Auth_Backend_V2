c0f012167127cc1e8833f06013692e5725928e8b
김범진 <sdpthf@naver.com>
2021-12-24 09:19:35 +0900

    📑 :: GlobalExceptionHandler

---
+++ resource-server/src/main/java/team/sb/resourceserver/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package team.sb.resourceserver.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import team.sb.resourceserver.global.error.exception.SBException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SBException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(SBException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new ErrorResponse(400, e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+}
