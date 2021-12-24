ccc0ad2423a8cc978c84793c22062d1084687d55
김범진 <sdpthf@naver.com>
2021-12-24 09:40:05 +0900

    📑 :: S3Util

---
+++ resource-server/src/main/java/team/sb/resourceserver/global/util/S3Util.java
@@ -0,0 +1,39 @@
+package team.sb.resourceserver.global.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import team.sb.resourceserver.global.config.S3Config;
+import team.sb.resourceserver.global.exception.InvalidImageException;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class S3Util {
+
+    private final S3Config s3Config;
+    private final AmazonS3Client amazonS3Client;
+
+    public String upload(MultipartFile image) {
+        String fileName = "nms/" + UUID.randomUUID() + image.getOriginalFilename();
+
+        try {
+            amazonS3Client.putObject(new PutObjectRequest(s3Config.getBucket(), fileName, image.getInputStream(), null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw InvalidImageException.EXCEPTION;
+        }
+
+        return fileName;
+    }
+
+    public String getFileUrl(String fileName) {
+        return amazonS3Client.getUrl(s3Config.getBucket(), fileName).toString();
+    }
+
+}
