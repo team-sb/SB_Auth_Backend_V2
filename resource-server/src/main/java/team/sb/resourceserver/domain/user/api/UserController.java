f6d136e6fc8cd526195a6dd6ed945180b97f576c
김범진 <sdpthf@naver.com>
2021-12-24 09:45:39 +0900

    📑 :: UserController

---
+++ resource-server/src/main/java/team/sb/resourceserver/domain/user/api/UserController.java
@@ -0,0 +1,24 @@
+package team.sb.resourceserver.domain.user.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import team.sb.resourceserver.domain.user.api.dto.EmailRequest;
+import team.sb.resourceserver.domain.user.service.UserService;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping("/image")
+    public void updateProfileImage(@RequestPart @Valid EmailRequest emailRequest,
+                                   @RequestPart MultipartFile multipartFile) {
+        userService.updateProfileImage(emailRequest.getEmail(), multipartFile);
+    }
+
+}
