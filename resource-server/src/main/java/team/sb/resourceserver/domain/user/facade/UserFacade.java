c7332c67e363b9b365d9c46abaad2c980684bfdb
김범진 <sdpthf@naver.com>
2021-12-24 11:42:00 +0900

    ⚡ :: getCurrentUser

--- resource-server/src/main/java/team/sb/resourceserver/domain/user/facade/UserFacade.java
+++ resource-server/src/main/java/team/sb/resourceserver/domain/user/facade/UserFacade.java
@@ -1,10 +1,13 @@
 package team.sb.resourceserver.domain.user.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import team.sb.resourceserver.domain.user.entity.User;
 import team.sb.resourceserver.domain.user.exception.UserNotFoundException;
 import team.sb.resourceserver.domain.user.repository.UserRepository;
+import team.sb.resourceserver.global.exception.AuthenticationNotFoundException;
+import team.sb.resourceserver.global.security.auth.AuthDetails;
 
 @RequiredArgsConstructor
 @Component
@@ -15,6 +18,17 @@ public class UserFacade {
     public User getByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public User getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(!(principal instanceof AuthDetails)) {
+            throw AuthenticationNotFoundException.EXCEPTION;
+        }
+
+        return ((AuthDetails) principal).getUser();
+
     }
 
 }
