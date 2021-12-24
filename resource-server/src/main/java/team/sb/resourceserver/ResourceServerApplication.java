02dc449fd606b0ae4003989f73d2fe4dd88c033b
김범진 <sdpthf@naver.com>
2021-12-24 09:18:38 +0900

    📑 :: JwtProperties

--- resource-server/src/main/java/team/sb/resourceserver/ResourceServerApplication.java
+++ resource-server/src/main/java/team/sb/resourceserver/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package team.sb.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ResourceServerApplication {
 
