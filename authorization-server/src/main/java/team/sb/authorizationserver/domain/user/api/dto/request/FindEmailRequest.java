f2a0da15ec1ecb7611b223cfafa8fbccb295ed59
김범진 <sdpthf@naver.com>
2022-01-01 22:48:46 +0900

    📑 :: findEmailRequest

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/user/api/dto/request/FindEmailRequest.java
@@ -0,0 +1,20 @@
+package team.sb.authorizationserver.domain.user.api.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class FindEmailRequest {
+
+    @NotBlank(message = "email은 Null, 공백을 허용하지 않습니다.")
+    @Email(message = "email 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "code는 Null, 공백을 허용하지 않습니다.")
+    private String code;
+
+}
