2b115fd50c1d0c6d1081ff2d30cc63ff01de8192
김범진 <sdpthf@naver.com>
2022-01-03 00:40:11 +0900

    ⚡ :: validateUser Controller

--- authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/sbauth/authorize").permitAll()
                 .antMatchers(HttpMethod.PUT,  "/sbauth/authorize").permitAll()
                 .antMatchers(HttpMethod.POST, "/sbauth/client").permitAll()
+                .antMatchers(HttpMethod.POST, "/sbauth/client-id").permitAll()
                 .antMatchers(HttpMethod.POST, "/sbauth/token").permitAll()
                 .anyRequest().authenticated()
 
