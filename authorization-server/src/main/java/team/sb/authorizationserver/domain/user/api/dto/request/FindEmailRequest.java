1c9f1a0895c59109737f395ac37c7cc822d10f89
김범진 <sdpthf@naver.com>
2022-01-10 11:59:03 +0900

    ⚡ :: Size로 변경

--- authorization-server/src/main/java/team/sb/authorizationserver/domain/user/api/dto/request/FindEmailRequest.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/user/api/dto/request/FindEmailRequest.java
@@ -2,16 +2,16 @@ package team.sb.authorizationserver.domain.user.api.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class FindEmailRequest {
 
     @NotBlank(message = "phoneNumber는 Null, 공백을 허용하지 않습니다.")
-    @Length(min = 11, max = 11, message = "phoneNumber는 11자여야합니다.")
+    @Size(min = 11, max = 11, message = "phoneNumber는 11자여야합니다.")
     private String phoneNumber;
 
     @NotBlank(message = "code는 Null, 공백을 허용하지 않습니다.")
