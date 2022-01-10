d7099acccede2c2b2ec926b7aff4b36a8edfcc09
김범진 <sdpthf@naver.com>
2022-01-10 11:59:47 +0900

    ⚡ :: applicationName 추가, scope 제거

--- authorization-server/src/main/java/team/sb/authorizationserver/domain/oauth/entity/OauthDetails.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/oauth/entity/OauthDetails.java
@@ -19,8 +19,8 @@ public class OauthDetails {
     @Column(columnDefinition = "char(11)", nullable = false)
     private String clientSecret;
 
-    @Column(columnDefinition = "char(30)", nullable = false)
-    private String scope;
+    @Column(columnDefinition = "char(20)", nullable = false)
+    private String applicationName;
 
     @Column(columnDefinition = "char(50)", nullable = false)
     private String webServerRedirectUri;
@@ -32,10 +32,10 @@ public class OauthDetails {
     @Enumerated(EnumType.STRING)
     private Role authorities;
 
-    public OauthDetails(String clientId, String clientSecret, String redirectUri, String scope) {
+    public OauthDetails(String clientId, String clientSecret, String redirectUri, String applicationName) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
-        this.scope = scope;
+        this.applicationName = applicationName;
         this.webServerRedirectUri = redirectUri;
         this.authorities = Role.ROLE_USER;
     }
