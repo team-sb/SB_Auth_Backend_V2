0c832ed4d0127e166bb46eae48b0f9b302b0d60a
김범진 <sdpthf@naver.com>
2022-01-10 12:01:35 +0900

    📑 :: RegisterClientRequest

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/oauth/api/dto/request/RegisterClientRequest.java
@@ -0,0 +1,20 @@
+package team.sb.authorizationserver.domain.oauth.api.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class RegisterClientRequest {
+
+    @NotBlank(message = "redirectUri는 Null, 공백을 허용하지 않습니다.")
+    private String redirectUri;
+
+    @NotBlank(message = "applicationName은 Null, 공백을 허용하지 않습니다.")
+    @Size(max = 20, message = "applicationName은 20글자 이하여야 합니다.")
+    private String applicationName;
+
+}
