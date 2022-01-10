9e9918f5b67f5e639bb9b70041280eadadd62327
김범진 <sdpthf@naver.com>
2022-01-10 12:14:08 +0900

    ⚰ :: /sbauth/client-id

--- authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/sbauth/authorize").permitAll()
                 .antMatchers(HttpMethod.PUT,  "/sbauth/authorize").permitAll()
                 .antMatchers(HttpMethod.POST, "/sbauth/client").permitAll()
-                .antMatchers(HttpMethod.POST, "/sbauth/client-id").permitAll()
                 .antMatchers(HttpMethod.POST, "/sbauth/token").permitAll()
                 .anyRequest().authenticated()
 
