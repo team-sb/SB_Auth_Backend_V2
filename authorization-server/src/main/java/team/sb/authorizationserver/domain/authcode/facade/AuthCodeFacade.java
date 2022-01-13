37d12cb3047b0e5e4ef81c81c923bb18f4cf985f
김범진 <sdpthf@naver.com>
2022-01-13 10:52:37 +0900

    ⚡ :: sendSms

--- authorization-server/src/main/java/team/sb/authorizationserver/domain/authcode/facade/AuthCodeFacade.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/authcode/facade/AuthCodeFacade.java
@@ -3,9 +3,12 @@ package team.sb.authorizationserver.domain.authcode.facade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import team.sb.authorizationserver.domain.authcode.entity.EmailAuthCode;
+import team.sb.authorizationserver.domain.authcode.entity.PhoneAuthCode;
 import team.sb.authorizationserver.domain.authcode.repository.EmailAuthCodeRepository;
+import team.sb.authorizationserver.domain.authcode.repository.PhoneAuthCodeRepository;
 import team.sb.authorizationserver.global.util.AuthUtil;
 import team.sb.authorizationserver.global.util.SesUtil;
+import team.sb.authorizationserver.global.util.SmsUtil;
 
 import java.util.Optional;
 
@@ -14,8 +17,10 @@ import java.util.Optional;
 public class AuthCodeFacade {
 
     private final SesUtil sesUtil;
+    private final SmsUtil smsUtil;
     private final AuthUtil authUtil;
     private final EmailAuthCodeRepository emailAuthCodeRepository;
+    private final PhoneAuthCodeRepository phoneAuthCodeRepository;
 
     public void sendEmail(String email) {
         String code = authUtil.getRandomCode(6);
@@ -31,6 +36,22 @@ public class AuthCodeFacade {
                 );
 
         sesUtil.sendEmail(email, code);
+    }
+
+    public void sendSms(String phoneNumber) {
+        String code = authUtil.getRandomCode(6);
+
+        phoneAuthCodeRepository.findById(phoneNumber)
+                .map(phoneAuthCode -> phoneAuthCode.update(code))
+                .or(() ->
+                        Optional.of(
+                                phoneAuthCodeRepository.save(
+                                        new PhoneAuthCode(phoneNumber, code)
+                                )
+                        )
+                );
+
+        smsUtil.sendCode(phoneNumber, code);
     }
 
 }
